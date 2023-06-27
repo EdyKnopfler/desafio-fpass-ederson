81814ad1fd13361e9ca8a14653f790bb6cde8971
Éderson Cássio <edersoncassio1983@gmail.com>
2023-06-26 23:32:14 -0300

    Todo o suporte para o NestJS acessar projetos 'de fora'

--- app/nestjs-api/src/main.ts
+++ app/nestjs-api/src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { sayHello } from '../../../core/use-case/example';
+
+console.log(sayHello());
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
