ec539c39aebb17a30f58a88a8ffd178839fbdc6d
Éderson Cássio <edersoncassio1983@gmail.com>
2023-07-01 14:57:24 -0300

    Listing heroes

---
+++ core/use-case/marvel.service.ts
@@ -0,0 +1,23 @@
+import * as crypto from 'crypto';
+import axios from 'axios';
+
+export class MarvelService {
+
+  private baseUrl = 'https://gateway.marvel.com/v1/public';
+  private privateKey = process.env.MARVEL_PRIVATE_KEY;
+  private publicKey = process.env.MARVEL_PUBLIC_KEY;
+
+  async listHeroes() {
+    const response = await axios.get(
+      `${this.baseUrl}/characters`, 
+      { params: { ...this.makeTsAndHash() } });
+    return response.data.data.results;
+  }
+
+  private makeTsAndHash() {
+    const ts = crypto.randomBytes(20).toString('hex');
+    const keys =  ts + this.privateKey + this.publicKey;
+    const hash = crypto.createHash('md5').update(keys).digest('hex');
+    return { ts, hash, apikey: this.publicKey };
+  }
+}
