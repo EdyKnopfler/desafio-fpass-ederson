77796b597acfc9cdef6ecd06b854a542b95b6430
Éderson Cássio <edersoncassio1983@gmail.com>
2023-07-01 15:07:43 -0300

    This isn't needed anymore

--- app/nestjs-api/src/main.ts
+++ app/nestjs-api/src/main.ts
@@ -2,10 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import '@js-joda/timezone';
 
-import { sayHello } from '../../../core/use-case/example';
-
-console.log(sayHello());
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
